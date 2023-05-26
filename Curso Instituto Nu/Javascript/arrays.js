1f812810abf901a1b8e92145cc7255fffa7214f0
Jardel "Archer" Sadala <jardelsadala899@gmail.com>
2023-05-26 09:31:56 -0400

    descrição

--- Curso Instituto Nu/Javascript/arrays.js
+++ Curso Instituto Nu/Javascript/arrays.js
@@ -24,35 +24,42 @@ for (var pos = 0 ; pos < valores.length; pos++){
 }
 
 var media = soma/valores.length;
-
 console.log(media);
 
 var arr1 = [1, 2, 3, 4, 5, 6];
-
 console.log(arr1.join(' '));
-
 var retirado = arr1.shift();
-
 console.log(retirado);
-
 console.log(arr1);
 
 console.log("Posição do número 3: " +arr1.indexOf(3));
 
 arr1.push(7);
 console.log(arr1);
-
 arr1.pop();
 console.log(arr1);
 
 var arr2 = [1,2,3,4,5,6,7];
-
 arr2.splice(2,3);
-
 console.log(arr2);
 
 var nomes = ["Maria", "João", "Lucas", "Pedro"];
-
-var novos = nomes.splice(1,1,"Luiz");
-
+var novos = nomes.splice(1,2,"Luiz", "Ronaldo");
 console.log(novos);
+
+var pais = ["Brasil", "Argentina", "Colombia"];
+pais.unshift("Uruguai");
+console.log(pais);
+
+
+var pessoa = ["Eduardo","Joana","Wallace","Rosana"];
+var gerente = ["David", "Manuela"];
+var pessoas1 = pessoa.slice(1.3);
+
+
+console.log(pessoa);
+console.log(pessoas1);
+
+var empresa = pessoa.concat(gerente);
+
+console.log(empresa);

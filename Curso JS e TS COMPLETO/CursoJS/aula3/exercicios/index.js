78bd17ce94fe3db6e89d0d90df9e4a399a63eefe
Jardel "Archer" Sadala <jardelsadala899@gmail.com>
2023-07-06 08:19:37 -0400

    update

---
+++ Curso JS e TS COMPLETO/CursoJS/aula3/exercicios/index.js
@@ -0,0 +1,1 @@
+console.log('Meu nome é "JARDEL". Estou aprendendo Javascript as 8 da manhã');
--- Curso JS e TS COMPLETO/CursoJS/aula3/index.js
+++ Curso JS e TS COMPLETO/CursoJS/aula3/index.js
@@ -1,3 +1,5 @@
 console.log("'Luiz Otávio'"); //string
 console.log('"Luiz Otávio"');
 console.log(`Luiz Otávio`);
+
+console.log(35, 15.85, 'Luiz Otávio de Novo');

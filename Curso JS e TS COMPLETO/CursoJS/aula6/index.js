942d493b4da5dc61a087853d68503cdd7501bc30
Jardel "Archer" Sadala <jardelsadala899@gmail.com>
2023-07-07 19:32:31 -0400

    update

---
+++ Curso JS e TS COMPLETO/CursoJS/aula6/index.js
@@ -0,0 +1,18 @@
+let nome = 'Henrique'; //string
+
+console.log(nome, 'nascem em 1984');
+console.log('Em 2000', nome, 'conheceu Maria');
+console.log(nome, 'casou-se com Maria em 2012');
+console.log('Maria teve um filho com',nome, 'em 2015');
+console.log('O filho de',nome, 'se chama Eduardo');
+
+/*
+*   Não pode começar o nome de uma variável com número
+*   Não podem conter espaços ou traços
+*   Não pode criar variáveis com palavras reservadas
+*   Variáveis precisam ter nomes significativos
+*   Utilizamos camelCase ex = let nomeCompletoDoCliente
+*   Case-sensitive
+*   Não podemos redeclarar variáveis com let
+*   Não utilize VAR, utilize LET
+*/
---
+++ Curso JS e TS COMPLETO/CursoJS/aula7/index.js
@@ -0,0 +1,20 @@
+//String = Text | Number = Número
+
+const primeiroNumero = 5; // Number
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+let resultadoTriplicado = resultado * 3;
+resultadoTriplicado = resultadoTriplicado + 5;
+console.log(typeof(primeiroNumero + segundoNumero))
+
+/*
+*   Não pode começar o nome de uma constante com número
+*   Não podem conter espaços ou traços
+*   Não pode criar constantes com palavras reservadas
+*   constantes precisam ter nomes significativos
+*   Utilizamos camelCase ex = let nomeCompletoDoCliente
+*   Case-sensitive
+*   Não pode modificar o valor de uma constante
+*   Não utilize VAR, utilize LET
+*/
